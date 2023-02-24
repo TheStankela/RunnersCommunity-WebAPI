a22fe8f35ad6e3ee69ac18ba3c7a9c3c7dfc7ee7
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-24 22:54:38 +0100

    Created Edit ViewModels for Clubs and Races.

---
+++ RunGroops/ViewModels/EditRaceViewModel.cs
@@ -0,0 +1,17 @@
+﻿using RunGroops.Data.Enum;
+using RunGroops.Models;
+
+namespace RunGroops.ViewModels
+{
+	public class EditRaceViewModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public IFormFile Image { get; set; }
+		public string? URL { get; set; }
+		public RaceCategory RaceCategory { get; set; }
+		public int? AddressId { get; set; }
+		public Address Address { get; set; }
+	}
+}
