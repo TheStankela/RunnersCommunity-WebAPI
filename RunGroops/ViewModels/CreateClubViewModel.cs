fc895f28390197178b9574a37184bbbd0c0481c3
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-24 20:37:44 +0100

    Created View Models when creating Clubs and Races.

---
+++ RunGroops/ViewModels/CreateClubViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RunGroops.Data.Enum;
+using RunGroops.Models;
+
+namespace RunGroops.ViewModels
+{
+    public class CreateClubViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public IFormFile Image { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+    }
+}
