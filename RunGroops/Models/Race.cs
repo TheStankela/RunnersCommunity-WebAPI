d9957a2208f57f0b833fa58020cbc0c25496797c
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-24 22:56:46 +0100

    AddressId is no longer nullable.

--- RunGroops/Models/Race.cs
+++ RunGroops/Models/Race.cs
@@ -18,7 +18,7 @@ namespace RunGroops.Models
 		public string? Facebook { get; set; }
 		public string? Contact { get; set; }
 		[ForeignKey("Address")]
-		public int AddressId { get; set; }
+		public int? AddressId { get; set; }
 		public Address Address { get; set; }
 		public RaceCategory RaceCategory { get; set; }
 		[ForeignKey("AppUser")]
