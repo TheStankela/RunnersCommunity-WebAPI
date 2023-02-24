d819b2b759c605b02baf77d381648dd93fe240e0
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-24 20:37:05 +0100

    Added Cloudinary services and settings.

---
+++ RunGroops/Interfaces/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace RunGroops.Interfaces
+{
+	public interface IPhotoService
+	{
+		public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+		Task<DeletionResult> DeletePhotoAsync(string publicId);
+	}
+}
--- RunGroops/Program.cs
+++ RunGroops/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using RunGroops.Data;
+using RunGroops.Helpers;
 using RunGroops.Interfaces;
 using RunGroops.Repository;
+using RunGroops.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
-
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
