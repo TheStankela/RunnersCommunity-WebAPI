55d90e188fe870f39c84b447bbab1ff7bdbe2e0d
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-26 21:37:04 +0100

    Added identity framework

--- RunGroops/Data/ApplicationDbContext.cs
+++ RunGroops/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RunGroops.Models;
 
 namespace RunGroops.Data
 {
-	public class ApplicationDbContext : DbContext
+	public class ApplicationDbContext : IdentityDbContext<AppUser>
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
 		{ 
