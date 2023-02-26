3e31892834db72c79987c00970a0947b0a3e52e1
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-26 21:38:00 +0100

    AddressId is now nullable

--- RunGroops/Models/AppUser.cs
+++ RunGroops/Models/AppUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RunGroops.Models
 {
@@ -6,6 +7,8 @@ namespace RunGroops.Models
 	{
 		public int? Pace { get; set; }
 		public int? Mileage { get; set; }
+		[ForeignKey("Address")]
+		public int? AddressId { get; set; }
 		public Address? Address { get; set; }
 		public ICollection<Club> Clubs { get; set; }
 		public ICollection<Race> Races { get; set; }
