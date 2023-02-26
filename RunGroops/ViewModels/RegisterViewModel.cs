a00cfa3a784ae323936c260ab1c095896c4e74f7
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-26 21:38:42 +0100

    ViewModel for AppUser Register

---
+++ RunGroops/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace RunGroops.ViewModels
+{
+	public class RegisterViewModel
+	{
+		[Display (Name = "Email address")]
+		[Required(ErrorMessage = "Email address is required.")]
+		public string EmailAddress { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[Display(Name = "Confirm Password")]
+		[Required(ErrorMessage = "Confirm password is required.")]
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "Passwords do not match.")]
+		public string ConfirmPassword { get; set; }
+	}
+}
