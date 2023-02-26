e7fb1a184d23143a59fd81e2615fb92cc6a4e456
TheStankela <ognjenogistankovic2002@gmail.com>
2023-02-26 21:42:35 +0100

    Added Identity Framework

--- RunGroops/Program.cs
+++ RunGroops/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RunGroops.Data;
 using RunGroops.Helpers;
 using RunGroops.Interfaces;
+using RunGroops.Models;
 using RunGroops.Repository;
 using RunGroops.Services;
 
@@ -16,13 +19,19 @@ builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection(
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+	.AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie();
 
 var app = builder.Build();
 
 if (args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-	//await Seed.SeedUsersAndRolesAsync(app);
-	Seed.SeedData(app);
+	await Seed.SeedUsersAndRolesAsync(app);
+	//Seed.SeedData(app);
 }
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
