c9e271907696e3c91342c3488a6276654951bf12
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-01 13:55:05 +0100

    Extension for ClaimsPrincipal to get the users ID

---
+++ RunGroops/Extensions/ClamsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace RunGroops.Extensions
+{
+	public static class ClamsPrincipalExtensions
+	{
+		public static string GetUserId (this ClaimsPrincipal user)
+		{
+			return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+		}
+	}
+}
