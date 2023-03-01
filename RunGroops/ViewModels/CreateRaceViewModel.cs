187201fbaf504423ed3ad45c7dd38dff6b4452cf
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-01 13:55:40 +0100

    Added AppUserId

--- RunGroops/ViewModels/CreateRaceViewModel.cs
+++ RunGroops/ViewModels/CreateRaceViewModel.cs
@@ -11,5 +11,6 @@ namespace RunGroops.ViewModels
 		public Address Address { get; set; }
 		public IFormFile Image { get; set; }
 		public ClubCategory RaceCategory { get; set; }
+		public string AppUserId { get; set; }
 	}
 }
