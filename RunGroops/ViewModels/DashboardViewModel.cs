3469acce4e5c12290eb89356d08e912c35a00736
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-01 13:55:51 +0100

    Added dashboard viewmodel

---
+++ RunGroops/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RunGroops.Models;
+
+namespace RunGroops.ViewModels
+{
+	public class DashboardViewModel
+	{
+		public List<Race> Races { get; set; }
+		public List<Club> Clubs { get; set; }
+
+	}
+}
