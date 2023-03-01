24363e7e2268f2f83974d284d8f4ade9c516c00c
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-01 13:55:22 +0100

    Dashboard repository

---
+++ RunGroops/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,10 @@
+﻿using RunGroops.Models;
+
+namespace RunGroops.Interfaces
+{
+	public interface IDashboardRepository
+	{
+		Task<List<Club>> GetAllUserClubs();
+		Task<List<Race>> GetAllUserRaces();
+	}
+}
---
+++ RunGroops/Repository/DashboardRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroops.Data;
+using RunGroops.Extensions;
+using RunGroops.Interfaces;
+using RunGroops.Models;
+
+namespace RunGroops.Repository
+{
+	public class DashboardRepository : IDashboardRepository
+	{
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly ApplicationDbContext _context;
+
+		public DashboardRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+		{
+			_httpContextAccessor = httpContextAccessor;
+			_context = context;
+		}
+
+		async Task<List<Club>> IDashboardRepository.GetAllUserClubs()
+		{
+			var curUser = _httpContextAccessor.HttpContext?.User.GetUserId();
+			var clubs = _context.Clubs.Where(c => c.AppUserId == curUser).Include(c => c.Address).ToList();
+
+			return clubs;
+
+		}
+
+		async Task<List<Race>> IDashboardRepository.GetAllUserRaces()
+		{
+			var curUser = _httpContextAccessor.HttpContext?.User.GetUserId();
+			var races = _context.Races.Where(c => c.AppUserId == curUser).Include(c => c.Address).ToList();
+
+			return races;
+		}
+	}
+}
