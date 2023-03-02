377833610e0b20cdf0ac4b1b2fb252487e67c22a
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-02 23:07:53 +0100

    User repository interface

---
+++ RunGroops/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using RunGroops.Models;
+
+namespace RunGroops.Interfaces
+{
+	public interface IUserRepository
+	{
+		public Task<IEnumerable<AppUser>> GetAllUsers();
+		public Task<AppUser> GetUserById(string id);
+
+		public bool AddUser(AppUser user);
+		public bool UpdateUser(AppUser user);
+		public bool DeleteUser(AppUser user);
+		public bool Save();
+	}
+}
