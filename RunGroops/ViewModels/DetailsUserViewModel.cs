6ad751aaf5063f37d05666470daf7f8ac46e34ec
TheStankela <ognjenogistankovic2002@gmail.com>
2023-03-02 23:08:12 +0100

    ViewModel for user details page

---
+++ RunGroops/ViewModels/DetailsUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RunGroops.ViewModels
+{
+	public class DetailsUserViewModel
+	{
+		public string Id { get; set; }
+		public string UserName { get; set; }
+		public int? Pace { get; set; }
+		public int? Mileage { get; set; }
+	}
+}
